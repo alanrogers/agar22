2597ac9e80f3966d5ffe74156527be007af5d34c
Alan R. Rogers <rogers@anthro.utah.edu>
2022-07-28 11:38:06 -0600

    add coal_polymorph.py

---
+++ legofit/coal_polymorph.py
@@ -0,0 +1,31 @@
+# Use computer simulation to estimate probability that a sample of
+# haploid size K is polymorphic, given constant population size twoN.
+from random import expovariate
+from math import exp
+
+twoN = 1000  # population size (number of gene copies)
+u = 1e-6     # mutation rate
+nitr = 1000  # number of iterations
+
+pr_mono = 0.0
+
+for i in range(nitr):
+    K = 30       # sample size (number of gene copies)
+    brlen = 0.0 # age of last common ancestor in generations
+
+    # Each pass through loop deals with one coalescent interval.
+    while K > 1:
+        h = K*(K-1)/(2.0*twoN) # hazard of a coalescent event
+        t = expovariate(h)       # time until next coalescent event
+        brlen += K*t
+        #print("%2d %7.2f %7.2f     REMOVE ME" % (K, t, brlen))
+        K -= 1
+
+    # 0 term of Poisson distribution give probability
+    # of zero mutations in this particular gene genealogy
+    pr_mono += exp(-u*brlen)
+
+# Mean probability that sample is monomorphic    
+pr_mono /= nitr
+
+print("Probability of polymorphism:", 1 - pr_mono)
