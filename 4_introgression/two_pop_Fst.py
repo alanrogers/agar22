54f81e2bc086d44d3a97fb368f1818e530e3b9a0
Shyamalika Gopalan <51133211+ShyamieG@users.noreply.github.com>
2022-07-28 16:03:38 -0400

    added migration back

--- 4_introgression/two_pop_Fst.py
+++ 4_introgression/two_pop_Fst.py
@@ -7,6 +7,7 @@ def main(args):
 	argp = ap.ArgumentParser(description="Calculate Fst for a two-population demographic model")
 	argp.add_argument("--Ne", type=int, default=1000, help="Effective population size of the descendent populations")
 	argp.add_argument("--t_split", type=int, default=200, help="Split time of descendent populations (generations ago)")
+	argp.add_argument("--mig_rate", type=float, default=0, help="Post-split migration rate between populations")
 	args = argp.parse_args(args)
 
 	demography = msprime.Demography()
@@ -14,12 +15,14 @@ def main(args):
 	demography.add_population(name="B", initial_size=args.Ne)
 	demography.add_population(name="C", initial_size=1e4)
 	demography.add_population_split(time=args.t_split, derived=["A", "B"], ancestral="C")
-	ts = msprime.sim_ancestry(samples={"A": 100, "B": 100}, sequence_length=1e6, recombination_rate=1e-7, demography=demography)
+	demography.add_symmetric_migration_rate_change(time=args.t_split, populations=["A", "B"], rate=args.mig_rate)
+	ts = msprime.sim_ancestry(samples={"A": 100, "B": 100}, sequence_length=1e6, demography=demography)
 	mts = msprime.sim_mutations(ts, rate=1e-8)
 
-	fst = mts.Fst(sample_sets=[range(0, 200), range(200, 400)])
+	fst = mts.Fst(sample_sets=[range(0, 200), range(201, 400)])
 	print("\nNe of descendent populations is " + str(args.Ne))
 	print("Populations split " + str(args.t_split) + " generations ago\n")
+	print("The post-split migraton rate is " + str(args.mig_rate) + " per generation (symmetric)\n")
 	print("   * Fst between descendent populations is " + str(np.around(fst, decimals=5)) + "\n")
 
 ## Run script
