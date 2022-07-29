5e6d3dfab2bed4fa8585f6b86ed218530002c587
Alan R. Rogers <rogers@anthro.utah.edu>
2022-07-28 21:59:20 -0600

    Removing bootstrap reps 10-49

--- legofit/europe/a/bash_pipeline.sh
+++ legofit/europe/a/bash_pipeline.sh
@@ -8,13 +8,13 @@
 #     bash bash_pipeline.sh
 
 # exit when any command fails
-set -e
+#set -e
 
 # keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+#trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 
 # echo an error message before exiting
-trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
+#trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
 
 # Stage 1 of analysis.
 bash a1.sh
